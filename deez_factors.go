53b10738dbf1e07f33d2aff0e79bc45c6eb23842
Kevin Thompson <kevin.david.thompson@gmail.com>
2016-01-12 20:28:34 -0600

    sort of working

--- deez_factors.go
+++ deez_factors.go
@@ -5,6 +5,8 @@ import (
   "log"
   "os"
   "github.com/joho/godotenv"
+  "github.com/google/go-github/github"
+  "golang.org/x/oauth2"
 )
 
 func main() {
@@ -14,5 +16,38 @@ func main() {
     log.Fatal("Error loading .env file")
   }
 
-  fmt.Println("Get Deez Factors!", os.Getenv("GITHUB_API_KEY"))
+  //authenticate to github
+  ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_API_KEY")})
+  tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+  // create a github client using the token from above
+  client := github.NewClient(tc)
+
+  // Get a list of org members that don't have 2FA enabled
+  options := &github.ListMembersOptions{Filter: "2fa_disabled"}
+  users, _, err := client.Organizations.ListMembers("heroku", options)
+
+  // Loop over the list of users and print their name
+  // User structs store values as pointers so we need to use
+  // the * to get the value
+  for _, v := range users {
+    fmt.Print(*v.Login, " - ")
+
+    if v.Name != nil {
+      fmt.Print(*v.Name)
+    } else {
+      fmt.Print("No Name")
+    }
+
+    fmt.Print(" - ")
+    if v.Email != nil {
+      fmt.Print(*v.Email)
+    } else {
+      fmt.Print("No Email")
+    }
+
+    fmt.Print("\n")
+  }
+
+
 }
